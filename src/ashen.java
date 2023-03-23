2e771998c8511b8ccdfcfa57ab575e773f01a63d
AshenPerera <Ashenofficial2002@gmail.com>
2023-03-23 07:28:13 +0530

    Java class Project codes ( Day 1)

---
+++ src/ashen.java
@@ -0,0 +1,11 @@
+public class ashen {
+    ashen (){
+        System.out.println(" JAVA");
+        String name = "Programming Language Is ";
+
+    }
+    public static void main(String[] args) {
+        ashen a = new ashen();
+
+    }
+}
